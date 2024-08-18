d83fbd2039e6cff5fba9b430464134cb7a6c771d
parisa <parisafarivash@gmail.com>
2024-08-18 11:58:38 +0330

    Added inline class in django admin

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,27 @@
+from webbrowser import register
+
 from django.contrib import admin
 
 from .models import Desk, Demand, Category, Item, DeskWaiter
 
 
 admin.site.register(Demand)
-admin.site.register(Desk)
-admin.site.register(Category)
 admin.site.register(Item)
 admin.site.register(DeskWaiter)
 
+class ItemAdminInline(admin.TabularInline):
+    model = Item
+
+
+@admin.register(Category)
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ItemAdminInline]
+
+
+class DemandAdminInline(admin.StackedInline):
+    model = Demand
+
+
+@admin.register(Desk)
+class DeskModelAdmin(admin.ModelAdmin):
+    inlines = [DemandAdminInline]
--- core/models/category.py
+++ core/models/category.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=64, null=False, blank=False)
-    parent = models.ForeignKey('self', on_delete=models.PROTECT)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, blank=True,
+                               null=True)
 
     def __str__(self):
         return f"{self.title}"
